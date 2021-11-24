f3d66ae75b235ad45cb774f24e299dc70a185bf4
Illensky <alexis.laroche.02240@gmail.com>
2021-11-24 16:21:03 +0100

    exercice done

--- exercice1.js
+++ exercice1.js
@@ -2,13 +2,20 @@ let monTexte = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do 
 
 // Completer le code suivant pour afficher la longueur de la variable monTexte
 
-alert(monTexte.);
+alert(monTexte.length);
 
 
 //Ajouter du code ci dessous pour réaliser la suite de l'exercice
 
+posSpan = document.querySelector('#position')
+let posI = monTexte.indexOf('i')
+posSpan.innerHTML = posI
 
 
+let stringMaj = monTexte.toUpperCase()
+let chaineMajSpan = document.querySelector('#chaineMaj')
+
+chaineMajSpan.innerHTML = stringMaj
 
 
 
